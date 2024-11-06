954b8e271903372567ddfd22a8a808cdde3dca41
abhay2133 <abhaybishthestudent@gmail.com>
2024-11-06 21:49:22 +0530

    deleted migrations

--- backend/prisma/migrations/20241106094751_m3/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `testcode` to the `Test` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Test" ADD COLUMN     "testcode" TEXT NOT NULL;
